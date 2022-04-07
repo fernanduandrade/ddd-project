83ff9e5b6e5fcb0fe7b5e3cee09174e0472e2f98
fernanduandrade <onanduandrade@gmail.com>
2022-04-06 22:19:47 -0300

    feat(Test): Adicionado tests para a api

---
+++ ApiTest/UserRepositoryTest/UserTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Moq;
+using Domain.Interfaces;
+using Domain.Entities;
+using Service.Services;
+using Service.Validators;
+
+namespace ApiTest.UserTest
+{
+    public class UserTest
+    {
+        [Fact]
+        public void CreateUserTest()
+        {
+            // Arrange Context
+            var repository = new Mock<IBaseRepository<User>>().Object;
+            var business = new BaseService<User>(repository);
+            User user = new User();
+
+            // Act
+            user.SetId(2);
+            user.Name = "Fernando";
+            user.Email = "onanduandrade@gmail.com";
+            user.Password = "123654";
+            var result = business.Add<UserValidator>(user);
+            // Assert
+            Assert.Equal(2, result.id);
+        }
+    }
+}
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -8,8 +8,14 @@ namespace Domain.Entities
 {
     public class User : BaseEntity
     {
-        public string Name { get; set; }   
-        public string Email { get; set; }
-        public string Password { get; set; }    
+        public string? Name { get; set; }   
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+        public User SetId(int cdUser)
+        {
+            this.id = cdUser;
+            return this;
+        }
     }
 }
